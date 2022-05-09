16261645594bd345bbc96a2dfd6bd66c9242bede
radekwarisch <r.warisch@gmail.com>
2022-05-09 20:59:53 +0200

    Request URL

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,5 +1,7 @@
 export async function middleware(request) {
   const country = request.geo.country || "US";
 
+  console.log("url", request.url);
+
   return new Response(`Hello, World. I'm in ${country}`);
 }
